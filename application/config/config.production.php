2ff1e0cbf7a96e9bd1f25d5ba9217e797064a18a
scienide00 <alexandar921@abv.bg>
2015-11-26 01:03:24 +0200

    Created core configuration and application files.

---
+++ application/config/config.production.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: scienide
+ * Date: 11/26/15
+ * Time: 12:06 AM
+ */
---
+++ application/core/Application.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: scienide
+ * Date: 11/25/15
+ * Time: 11:00 PM
+ */
