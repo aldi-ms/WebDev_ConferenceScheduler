48e0597ed948bf654f4b0a448b8f5244a07a1e1d
scienide00 <alexandar921@abv.bg>
2015-11-26 20:44:02 +0200

    Added DbFactory class

---
+++ application/core/DbFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types = 1);
+
+class DbFactory
+{
+    private static $factory;
+    private $database;
+
+    public static function getFactory()
+    {
+        if (!self::$factory) {
+            self::$factory = new DbFactory();
+        }
+        return self::$factory;
+    }
+
+    public function getConnection()
+    {
+        if (!$this->database) {
+            $options = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ, PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING);
+            $this->database = new PDO(
+                Config::get('DB_TYPE') . ':host=' . Config::get('DB_HOST') . ';dbname=' .
+                Config::get('DB_NAME') . ';port=' . Config::get('DB_PORT') . ';charset=' . Config::get('DB_CHARSET'),
+                Config::get('DB_USER'), Config::get('DB_PASS'), $options
+            );
+        }
+        return $this->database;
+    }
+}
